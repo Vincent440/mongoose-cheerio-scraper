dca85ae81073afb1181ad2419c314185679e74cc
Vince216 <vinceshury@gmail.com>
2019-06-04 11:48:44 -0400

    Creating partials and setting up overall file structure to display the article data from the database

--- public/js/app.js
+++ public/js/app.js
@@ -1,2 +1,45 @@
 // JS file to store the user input for notes
 console.log("Connected!");
+
+// Shorthand for $( document ).ready()
+$(()=> {
+
+    /*
+    // When the user scrolls down 20px from the top of the document, show the button
+    window.onscroll = function() {
+        scrollFunction()
+    };
+
+    function scrollFunction() {
+        if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+            document.$("#scrollBtn").css("display","block");
+        } else {
+            document.$("#scrollBtn").css("display","none");
+        }
+    }
+
+    // When the user clicks on the button, scroll to the top of the document
+    function topFunction() {
+        document.body.scrollTop = 0; // For Safari
+        document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+    }
+*/
+
+    // When the user scrolls down 20px from the top of the document, show the button
+    window.onscroll = function() {scrollFunction()};
+
+    function scrollFunction() {
+        if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+            document.getElementById("myBtn").style.display = "block";
+        } else {
+            document.getElementById("myBtn").style.display = "none";
+        }
+    }
+
+    // When the user clicks on the button, scroll to the top of the document
+    function topFunc() {
+        document.body.scrollTop = 0; // For Safari
+        document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+    }
+
+});
